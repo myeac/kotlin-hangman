f224659083b0ecc57478fec0e45e49ba3cafffc7
MiguelY <miguelyeac@hotmail.com>
2023-09-18 08:35:58 -0300

    print positions

--- src/main/kotlin/TicTacToe.kt
+++ src/main/kotlin/TicTacToe.kt
@@ -1,3 +1,4 @@
+[
 var board = arrayListOf<ArrayList<String>>()
 
 fun main(args: Array<String>) {
@@ -9,7 +10,36 @@ fun main(args: Array<String>) {
         }
         board.add(row)
     }
+
     printBoard()
+
+    //game deevelopment
+
+    var continueGame = true
+
+    do {
+        println("ingresar una posicion (eg 1, 1)")
+
+        val input = readlnOrNull() ?: ""
+        var x = 0
+        var y = 0
+        try {
+            val positions = input.split(",")
+            x = positions[0].trim().toInt()
+            y = positions[1].trim().toInt()
+            if (board[x - 1][y - 1] != "") {
+                println("thata position is alreade taken, try again")
+            } else {
+                board[x - 1][y - 1] = "X"
+                printBoard()
+            }
+
+        } catch (e: Exception) {
+            println("invalid input, please try again")
+        }
+
+    } while (continueGame)
+
 }
 
 fun printBoard() {
